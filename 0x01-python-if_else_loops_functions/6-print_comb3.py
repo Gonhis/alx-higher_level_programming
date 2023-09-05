4e0902fa6c0dd0e50e5aeeb5ee5f3c2117bf44c1
gonhis <h11mh12is@gmail.com>
2023-09-05 15:22:55 -0700

    task 6 done

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for DG1 in range(10):
+    for DG2 in range(DG1 + 1, 10):
+        if DG1 == 0 and DG2 == 1:
+            print("01", end=",\n")
+        else:
+            print("{:02}".format(DG1 * 10 + DG2), end=",\n")
