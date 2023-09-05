85b570d79ff1435646c36ebd5e180cbeb489b909
gonhis <h11mh12is@gmail.com>
2023-09-05 15:36:08 -0700

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ for DG1 in range(10):
     for DG2 in range(DG1 + 1, 10):
         if DG1 == 0 and DG2 == 1:
             print("01", end=", ")
-        elif:
-            (DG1 * 10 + DG2) == 89:
-                print("{}".format(DG1 * 10 + DG2), end=",\n")
-            else:
-                print("{:02}".format(DG1 * 10 + DG2), end=", ")
+        elif (DG1 * 10 + DG2) == 89:
+            print("{}".format(DG1 * 10 + DG2), end=",\n")
+        else:
+            print("{:02}".format(DG1 * 10 + DG2), end=", ")
